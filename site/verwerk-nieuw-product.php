d11d7e4db34e0202bca1561d2b22b4a3fc0e5b61
Saabbass <178414@novacollege.nl>
2023-04-12 13:27:36 +0200

    les2b

--- site/verwerk-nieuw-product.php
+++ site/verwerk-nieuw-product.php
@@ -1,3 +1,18 @@
-<pre>
-    <?php var_dump($_POST);?>
-</pre>
+<?php
+    // variabele declareren
+    $naam = $_POST['naamProduct'];
+    $category = $_POST['categoryProduct'];
+    $prijs = $_POST['prijsProduct'];
+    $merk = $_POST['merkProduct'];
+
+    require 'database.php';
+
+    $sql = "INSERT INTO tools (tool_name, tool_category, tool_price, tool_brand)
+    VALUES ($naam, $category, $prijs, $merk)";
+
+    // Voer de INSERT INTO STATEMENT uit
+    mysqli_query($conn, $sql);
+
+    echo "Inserted successfully";
+    mysqli_close($conn); // Sluit de database verbinding
+?>
