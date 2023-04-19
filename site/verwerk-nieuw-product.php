1f7d7d6e2f795c572fea65c038f7dd5d7e0aa36c
Saabbass <178414@novacollege.nl>
2023-04-19 13:21:55 +0200

    beveiligen

--- site/verwerk-nieuw-product.php
+++ site/verwerk-nieuw-product.php
@@ -1,11 +1,21 @@
 <?php
+    require 'database.php';
+
+    // var_dump($_SERVER["REQUEST_METHOD"]);
+    // die;
+
+    if($_SERVER["REQUEST_METHOD"] !== "POST"){
+        // echo 'JE MAG HIER NIET KOMEN MET DIT REQUEST TYPE';
+        header($_SERVER["SERVER_PROTOCOL"] . " 405 Method Not Allowed", true, 405);
+        include '405.php';
+        exit;
+    }
+
     // variabele declareren
     $naam = $_POST['naamProduct'];
     $category = $_POST['categoryProduct'];
     $prijs = $_POST['prijsProduct'];
     $merk = $_POST['merkProduct'];
-
-    require 'database.php';
 
     $sql = "INSERT INTO tools (tool_name, tool_category, tool_price, tool_brand)
     VALUES ('$naam', '$category', '$prijs', '$merk')";
