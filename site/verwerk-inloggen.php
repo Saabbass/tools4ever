34a4e70553c245415192673fe88371790c510895
Saabbass <178414@novacollege.nl>
2023-04-19 13:43:06 +0200

    verwerk-inloggen

--- site/verwerk-inloggen.php
+++ site/verwerk-inloggen.php
@@ -1,3 +1,38 @@
 <?php
 
-    
+    require 'database.php';
+
+    function validate($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    $email = validate($_POST['email']);
+    $pass = validate($_POST['password']);
+
+    // $email = $_POST['email'];
+    // $pass = $_POST['password'];
+
+    $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$pass'";
+
+    $result = mysqli_query($conn, $sql);
+
+    $user = mysqli_fetch_assoc($result);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        Welkom <?php echo $user['firstname']; ?>
+    </h1>
+</body>
+</html>
