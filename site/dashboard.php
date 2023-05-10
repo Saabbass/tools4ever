ae8d236fdafce91d25a74c6719f1a1229098a030
Saabbass <178414@novacollege.nl>
2023-05-10 09:33:12 +0200

    logout.php add

--- site/dashboard.php
+++ site/dashboard.php
@@ -5,4 +5,20 @@ if(!isset($_SESSION['isIngelogd'])){
     exit;
 }
 
-echo "welkom" .$_SESSION['voornaam'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo "welkom" .$_SESSION['voornaam']; ?></h1>
+    <div>
+        <a href="logout.php">logout</a>
+    </div>
+</body>
+</html>
